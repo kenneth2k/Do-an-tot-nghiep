7cb789d9587445d45f802b263c046faae6cb1dad
TrongNV <trong.nguyenvan@haravan.com>
2023-01-17 11:32:48 +0700

    fix bug vercel

--- src/public/admin/js/message.js
+++
@@ -1,7 +0,0 @@
-const message = {
-    addSuccess: `Thêm thành công!`,
-    addFailed: `Thêm thất bại!`,
-    updateSuccess: `Cập nhật thành công!`,
-    updateFailed: `Cập nhật thất bại!`,
-    deleteSuccess: `Xóa thành công`
-}
