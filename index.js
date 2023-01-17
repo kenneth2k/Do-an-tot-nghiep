af1e7f5a84e896ee61186c072122d9bb7f007598
TrongNV <trong.nguyenvan@haravan.com>
2023-01-17 12:05:44 +0700

    fix deloy

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const handlebars = require("express-handlebars");
+const route = require("./src/app/routes");
+const db = require("./src/app/config/db");
 const dotenv = require("dotenv");
+const blocks = require("./src/helper/blocks");
+// const morgan = require('morgan');
+// Connect .env
 dotenv.config();
+// Connect Database
+db.connect();
+
+const app = express();
 const port = process.env.PORT || 3300;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Http logger
+// app.use(morgan('combined'));
+//static file
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+//middleware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// override with POST having
+app.use(methodOverride("_method"));
+
+//view engine
+const helper = {
+  extname: ".hbs",
+  helpers: blocks,
+};
+app.engine("hbs", handlebars(helper));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "app/views"));
+
+//Route
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Connected Port:", `http://localhost:${port}`);
 });
